b2c6f79a92ac229cf9b66936ae4b07693ac6a1ca
Aqil Səidov <aqilasa638@gmail.com>
2025-09-17 23:31:19 +0400

    LeetCode Problem 2 Solved

--- Turing_LeetCode2/src/main/java/projects/Solutions.java
+++ Turing_LeetCode2/src/main/java/projects/Solutions.java
@@ -126,4 +126,39 @@ public class Solutions {
 
     }
 
+    // LeetCode 2. Add Two Numbers
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode start = new ListNode(0);
+        int sum = 0;
+        int carry = 0;
+        ListNode current = start;
+
+        while(l1 != null || l2 != null || carry != 0) {
+            int val1 = 0;
+            int val2 = 0;
+
+            if(l1 != null){
+                val1 = l1.val;
+            }else {
+                val1 = 0;
+            }
+
+            if(l2 != null){
+                val2 = l2.val;
+            }else {
+                val2 = 0;
+            }
+
+            sum = val1 + val2 + carry;
+            carry = sum/10;
+
+            current.next = new ListNode(sum%10);
+            current = current.next;
+
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
+        }
+        return start.next;
+    }
+
 }
