6e90ebba5b2202d75d5f004542cadea0f20ec99b
Aqil Səidov <aqilasa638@gmail.com>
2025-09-17 23:11:50 +0400

    ListNode class implemented and Problem 206 solved

---
+++ Turing_LeetCode2/src/main/java/projects/Main.java
@@ -0,0 +1,8 @@
+package projects;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- Turing_LeetCode2/src/main/java/projects/Solutions.java
+++ Turing_LeetCode2/src/main/java/projects/Solutions.java
@@ -1,4 +1,19 @@
 package projects;
 
+
 public class Solutions {
+
+    //LeetCode 206. Reverse LL
+    public ListNode reverseList(ListNode head) {
+        ListNode temp = head;
+        ListNode pre = null;
+
+        while(temp != null){
+            ListNode nextNode = temp.next;
+            temp.next = pre;
+            pre = temp;
+            temp = nextNode;
+        }
+        return pre;
+    }
 }
