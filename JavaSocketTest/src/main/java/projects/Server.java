98296beb2df2151c27aafd4c6110bb15cd9fd69b
Aqil Səidov <aqilasa638@gmail.com>
2025-09-17 23:10:35 +0400

    Java Socket Tested and Client-Server messaging implemented

--- JavaSocketTest/src/main/java/projects/Server.java
+++ JavaSocketTest/src/main/java/projects/Server.java
@@ -4,8 +4,7 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Server {
     public static void main(String[] args) throws IOException {
         Socket socket = null;
