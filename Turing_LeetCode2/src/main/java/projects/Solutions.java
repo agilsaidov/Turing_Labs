d177f7f0d0e858dbcdea647c293704f096eb8d2f
Aqil Səidov <aqilasa638@gmail.com>
2025-09-18 23:44:39 +0400

    LeetCode problem 92 solved

--- Turing_LeetCode2/src/main/java/projects/Solutions.java
+++ Turing_LeetCode2/src/main/java/projects/Solutions.java
@@ -220,4 +220,33 @@ public class Solutions {
 
     }
 
+
+    //Leetcode 92. Reverse Linked List2
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null || left == right) return head;
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+        for (int i = 1; i < left; i++) {
+            prev = prev.next;
+        }
+
+        ListNode curr = prev.next;
+        ListNode next = null;
+        ListNode tail = curr;
+
+        for (int i = left; i <= right; i++) {
+            next = curr.next;
+            curr.next = prev.next;
+            prev.next = curr;
+            curr = next;
+        }
+
+        tail.next = curr;
+
+        return dummy.next;
+    }
+
 }
