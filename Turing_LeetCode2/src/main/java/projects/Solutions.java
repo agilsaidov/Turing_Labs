c21b462912b7f32d23fe8ca31ccdf995aea75e0e
Aqil Səidov <aqilasa638@gmail.com>
2025-09-18 23:35:28 +0400

    LeetCode problem 82 solved

--- Turing_LeetCode2/src/main/java/projects/Solutions.java
+++ Turing_LeetCode2/src/main/java/projects/Solutions.java
@@ -179,4 +179,30 @@ public class Solutions {
         return maxArea;
     }
 
+
+    //LeetCode 82. Remove Duplicates from Sorted List 2
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode dummy = new ListNode(0, head);
+        ListNode pre = dummy;
+        ListNode cur = head;
+
+        while (cur != null) {
+            while (cur.next != null && cur.val == cur.next.val) {
+                cur = cur.next;
+            }
+
+            if (pre.next == cur) {
+                pre = pre.next;
+            } else {
+                pre.next = cur.next;
+            }
+
+            cur = cur.next;
+        }
+
+        return dummy.next;
+    }
+
 }
