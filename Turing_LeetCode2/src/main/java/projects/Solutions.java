651de35fceccbae96f849e3a588b9eab386ba600
Aqil Səidov <aqilasa638@gmail.com>
2025-09-28 23:46:07 +0400

    Leetcode Problem 27. Remove Element solved

--- Turing_LeetCode2/src/main/java/projects/Solutions.java
+++ Turing_LeetCode2/src/main/java/projects/Solutions.java
@@ -305,4 +305,17 @@ public class Solutions {
         return length;
     }
 
+
+    //27. Remove Element
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        for(int i = 0 ; i<nums.length ; i++){
+            if(nums[i] != val){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+
 }
