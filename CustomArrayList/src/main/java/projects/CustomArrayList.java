4d6cd15b764da5eb997626634049a294d9cc81fc
Aqil Səidov <aqilasa638@gmail.com>
2025-10-05 16:56:25 +0400

    Implement addFirst(), add(index, obj), size() and helper indexShift(index) methods

--- CustomArrayList/src/main/java/projects/CustomArrayList.java
+++ CustomArrayList/src/main/java/projects/CustomArrayList.java
@@ -14,12 +14,33 @@ public class CustomArrayList<T> {
         array[size++] = t;
     }
 
+    public void add(int index, T t) {
+        if(index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index);
+        }
+        indexShift(index);
+        array[index] = t;
+        size++;
+    }
+
+    public void addFirst(T t) {
+        indexShift(0);
+        array[0] = t;
+        size++;
+    }
+
     public T get(int index) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("Index: " + index);
         }
         return array[index];
     }
+
+    public int size(){
+        return size;
+    }
+
+
 
 
 
@@ -30,6 +51,13 @@ public class CustomArrayList<T> {
             T[] newArray = (T[]) new Object[array.length + 20];
             System.arraycopy(array, 0, newArray, 0, array.length);
             array = newArray;
+        }
+    }
+
+    private void indexShift(int index) {
+        sizeChecker();
+        for (int i = size; i > index; i--) {
+            array[i] = array[i - 1];
         }
     }
 
